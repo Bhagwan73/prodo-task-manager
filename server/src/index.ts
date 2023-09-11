37d331443168031c16862547df0a977bf0332fd6
Bhagwan73 <bhagwan735025@gmail.com>
2023-09-11 09:55:16 +0530

    Access-Control-Allow-Origin header removed

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,13 +20,7 @@ app.use(cors({
     origin: "*",
     credentials: true
 }))
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); 
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+
 
 app.use("/", route)
 const PORT = process.env.PORT || 3001
